e94aef06a8ba70a5f01def496af9f611eccc9f6a
Brandon Wang <sourbrandon@gmail.com>
2020-04-03 00:04:50 -0700

    add footer components

---
+++ client/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div>
+      <hr/>
+      <small>built by <a href="https://brandon-wang.me">brandon wang</a></small>
+      <br/>
+      <br/>
+    </div>
+  );
+}
+
+export default Footer;
