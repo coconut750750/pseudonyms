7773e4aeee722c1b722e87956209dd3f692860bb
Brandon Wang <sourbrandon@gmail.com>
2020-04-22 01:26:01 -0700

    minor changes

--- client/src/components/Donate.js
+++ client/src/components/Donate.js
@@ -10,8 +10,7 @@ const altPrompts = [
 ];
 
 function getPrompt() {
-  const r = Math.floor(Math.random() * 2);
-  if (r === 0) {
+  if (Math.random() < 0.5) {
     return "Like the app? Support your local broke college student";
   } else {
     const i = Math.floor(Math.random() * altPrompts.length);
@@ -27,7 +26,7 @@ export default function Donate(props) {
     return (
       <div>
         <div className="donate alert alert-info" role="alert">
-          <a href="https://www.paypal.me/brandonw4">{prompt}</a>
+          <a href="https://www.paypal.me/brandonw4" target="_blank" rel="noopener noreferrer">{prompt}</a>
 
           <button className="badge-x" onClick={() => setDisplay(false)}>
             <span/>
