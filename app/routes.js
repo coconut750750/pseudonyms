83ef58683dc9e5f5d6bcc4d91417e3b00ace5d47
Brandon Wang <sourbrandon@gmail.com>
2020-06-28 00:54:14 -0700

    fix blocking all ips

--- app/routes.js
+++ app/routes.js
@@ -15,7 +15,7 @@ router.get('/wordlists', (req, res) => {
 });
 
 const feedbackLimiter = rateLimit({
-  windowMs: 24 * 60 * 60 * 1000, // 1 day window
+  windowMs: 60 * 60 * 1000, // 1 hour window
   max: 3, // start blocking after 3 requests
   message: JSON.stringify({ message: "Too many feedback submissions from this IP, please try again tomorrow." }),
 });
