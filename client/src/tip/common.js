1aefcf67baaef3eed34cce5039c5fe885cb3c35f
Brandon Wang <sourbrandon@gmail.com>
2020-07-03 00:30:56 -0700

    update tip

--- client/src/tip/common.js
+++ client/src/tip/common.js
@@ -1,4 +1,4 @@
-export const timeForClue = "A blank clue is presented when this timer runs out."
+export const timeForClue = "No clue is shown when this timer runs out."
 export const timeForGuess = "The turn immediately ends when this timer runs out."
 export const customWords = "Customize your game by uploading your own list of words! (Min. 50 words)";
 export const teamClue = "This is your clue! The number tells how many words on the board relate to this clue.";
