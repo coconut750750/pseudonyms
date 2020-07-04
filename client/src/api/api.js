a01f2f6d44e256bb8161b276ef390a3266e22cb6
Brandon Wang <sourbrandon@gmail.com>
2020-07-04 10:51:46 -0700

    revert client api interface

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -3,7 +3,7 @@ export async function callApi(path, data) {
   const body = await response.json();
 
   if (response.status !== 200) {
-    throw Error(body?.message);
+    throw Error(body.message);
   }
 
   return body;
