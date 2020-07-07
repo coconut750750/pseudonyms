24ce5d7977d39fab3a54006f63e25a610f16d3b8
Brandon Wang <sourbrandon@gmail.com>
2020-07-07 00:09:14 -0700

    fix emoji testing

--- client/src/game_components/TileText.js
+++ client/src/game_components/TileText.js
@@ -1,9 +1,20 @@
 import React from 'react';
+const _ = require("lodash");
 
-var emojiRe = /^(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])+$/
+var emojiRe = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/
+
+const onlyEmoji = (text) => {
+  const chars = _.toArray(text);
+  for (let c of chars) {
+    if (!emojiRe.test(c)) {
+      return false;
+    }
+  }
+  return true;
+}
 
 export default function TileText(props) {
-  const emojiClass = emojiRe.test(props.text);
+  const emojiClass = onlyEmoji(props.text);
   const textClass = emojiClass ? "emoji" : "text";
 
   return (
