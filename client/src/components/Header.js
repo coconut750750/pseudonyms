d75cdfceea78486bd24c8fce024efccf49a51301
Brandon Wang <sourbrandon@gmail.com>
2020-07-10 22:00:19 -0700

    refresh page when click header to ensure home page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,7 +6,7 @@ import './Header.css';
 export default function Header(props) {
   return (
     <div id="header">
-      <Link to="/" tabIndex="-1"><h3>Pseudonyms</h3></Link>
+      <a href="/" tabIndex="-1"><h3>Pseudonyms</h3></a>
       <div className="row">
         <div className="col-2 d-flex justify-content-center">{props.tipToggle}</div>
         <div className="col-8"><h6>Codenames online</h6></div>
