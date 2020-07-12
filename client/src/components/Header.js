6d9728187eda7099a5f90eec6302eb65e47967ce
Brandon Wang <sourbrandon@gmail.com>
2020-07-11 19:22:36 -0700

    update game header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 
 import './Header.css';
 
 export default function Header(props) {
   return (
     <div id="header">
-      <a href="/" tabIndex="-1"><h3>Pseudonyms</h3></a>
       <div className="row">
-        <div className="col-2 d-flex justify-content-center">{props.tipToggle}</div>
+        <div className="col-2 d-flex justify-content-center"></div>
+        <div className="col-8"><a href="/" tabIndex="-1"><h3>Pseudonyms</h3></a></div>
+        <div className="col-2 d-flex justify-content-center flex-column">{props.userButton}</div>
+      </div>
+
+      <div className="row">
+        <div className="col-2 d-flex justify-content-center"></div>
         <div className="col-8"><h6>Codenames online</h6></div>
-        <div className="col-2 d-flex justify-content-center">{props.userButton}</div>
+        <div className="col-2 d-flex justify-content-center">{props.tipToggle}</div>
       </div>
       <hr/>
     </div>
