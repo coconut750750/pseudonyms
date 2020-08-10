0a9854a37cc4540673a5052022b8a027499f1bac
Brandon Wang <sourbrandon@gmail.com>
2020-08-10 16:53:23 -0700

    store dark mode in localstorage

--- client/src/components/DarkModeToggle.js
+++ client/src/components/DarkModeToggle.js
@@ -1,24 +1,26 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Toggle from '../input_components/Toggle';
 
 import './DarkModeToggle.css';
 
 export default function DarkModeToggle(props) {
-  const [darkModeOn, setDarkModeOn] = useState(false);
+  const [darkModeOn, setDarkModeOn] = useState((localStorage.getItem("dark") || 'false') === 'true');
 
-  const toggleDarkMode = useCallback(() => {
+  useEffect(() => {
+    localStorage.setItem("dark", darkModeOn);
     if (darkModeOn) {
-      document.body.classList.remove("dark");
-    } else {
       document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
     }
-    setDarkModeOn(!darkModeOn);
   }, [darkModeOn]);
 
   return (
     <div id="dark-mode-toggle">
-      <Toggle onClick={toggleDarkMode}/>
+      <Toggle
+        onClick={() => setDarkModeOn(!darkModeOn)}
+        clicked={darkModeOn}/>
     </div>
   );
 }
