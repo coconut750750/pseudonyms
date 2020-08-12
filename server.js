7aff0e4585fd9f482bfea3ef6b724f32410a1d74
Brandon Wang <sourbrandon@gmail.com>
2020-08-11 17:53:13 -0700

    move client build in postbuild to prevent client downtime during build

--- server.js
+++ server.js
@@ -87,10 +87,10 @@ app.io.on('connect', function (socket) {
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, 'client/dist')));
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
     });
 }
 
