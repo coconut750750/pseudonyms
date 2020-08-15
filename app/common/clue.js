5695a3ad275357dc440c8207ec9317f9cf2e1f18
Brandon Wang <sourbrandon@gmail.com>
2020-08-14 17:20:09 -0700

    add mongoose to clue

--- app/common/clue.js
+++ app/common/clue.js
@@ -1,5 +1,20 @@
-class Clue {
+const mongoose = require('mongoose');
+
+class ClueSchema extends mongoose.Schema {
+  constructor() {
+    super();
+    mongoose.Schema.apply(this, arguments);
+    this.add({
+      word: {type: String, required: true},
+      count: {type: Number, required: true},
+      team: {type: String, required: true},
+    });
+  }
+}
+
+class Clue extends mongoose.Model {
   constructor(word, count, team) {
+    super();
     this.word = word;
     this.count = count;
     this.team = team;
@@ -10,4 +25,8 @@ class Clue {
   }
 }
 
-module.exports = Clue;
+const schema = new ClueSchema();
+schema.loadClass(Clue);
+const ClueModel = mongoose.model(Clue, schema);
+
+module.exports = ClueModel;
