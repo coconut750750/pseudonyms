b028e5530222e00a3bc508831cab5cb96fe36d20
Brandon Wang <sourbrandon@gmail.com>
2021-06-12 16:53:12 -0700

    set tabindex to -1 for toggles

--- client/src/input_components/Toggle.js
+++ client/src/input_components/Toggle.js
@@ -5,7 +5,7 @@ import './Toggle.css'
 export default function Toggle(props) {
   return (
     <label className="switch">
-      <input type="checkbox" checked={props.clicked} onClick={props.onClick}/>
+      <input tabindex="-1" type="checkbox" checked={props.clicked} onClick={props.onClick}/>
       <span className="slider round"></span>
     </label>
   );
