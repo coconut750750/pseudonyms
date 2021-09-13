b5c6c27619da49693047f86f78ad2c6fb1978485
Brandon Wang <sourbrandon@gmail.com>
2021-09-12 22:36:34 -0700

    fix warning

--- client/src/input_components/Toggle.js
+++ client/src/input_components/Toggle.js
@@ -5,7 +5,7 @@ import './Toggle.css'
 export default function Toggle(props) {
   return (
     <label className="switch">
-      <input tabindex="-1" type="checkbox" checked={props.clicked} onClick={props.onClick}/>
+      <input tabIndex="-1" type="checkbox" checked={props.clicked} onClick={props.onClick}/>
       <span className="slider round"></span>
     </label>
   );
