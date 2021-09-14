e28af95a075d5d277762162417ac9fe3b67acb56
Brandon Wang <sourbrandon@gmail.com>
2021-09-14 00:16:32 -0700

    add vertical lines for guess end

---
+++ client/src/components/ScoreTrend.js
@@ -0,0 +1,153 @@
+import React from 'react';
+import { LinearScale } from 'chart.js';
+import { Chart, Line } from 'react-chartjs-2';
+
+import {
+  STYLES,
+} from '../utils/const';
+
+class ScoreTrendScale extends LinearScale {
+  buildTicks() {
+    return this.chart.data.annotations.map(v => ({
+      value: v.value,
+      label: [v.annotation],
+      major: false,
+    }));
+  }
+
+  generateTickLabels(ticks) {
+    // label already generated in buildTicks, override for noop
+  }
+}
+
+ScoreTrendScale.id = 'scoreTrendScale';
+Chart.register(ScoreTrendScale);
+
+const generateLabels = (guessEndTimes) => ([0, ...guessEndTimes.map(t => t)]);
+
+const clueToAnnotation = (clue) => (`${clue.word} : ${clue.count}`);
+
+const generateAnnotations = (clueHistory, clueEndTimes, suffix) => {
+  const annotations = clueHistory.map(c => clueToAnnotation(c)).concat(suffix);
+  return clueEndTimes.map((t, i) => ({ value: t, annotation: annotations[i] }));
+};
+
+const generateDataset = (name, color, trend) => ({
+  label: name,
+  fill: false,
+  tension: 0.1,
+  backgroundColor: color,
+  borderColor: color,
+  borderWidth: 3,
+  data: trend,
+  pointRadius: 0,
+});
+
+const generateXAxisStyle = (color, max) => ({
+  min: 0,
+  max: max,
+  type: 'scoreTrendScale',
+  ticks: {
+    padding: 0,
+    font: {
+      family: STYLES.font,
+      size: STYLES.fontSize,
+    },
+    color,
+    autoSkip: false,
+    maxRotation: 30,
+    minRotation: 30,
+  },
+  grid: {
+    // display: false,
+    drawBorder: false,
+    drawOnChartArea: false,
+    color: 'black',
+  },
+  afterDataLimits: function(axis) {
+    // add inner padding for points on the far right edge
+    const max = axis.max;
+    // console.log(axis.min, axis.max)
+    axis.min -= max * .01;
+    axis.max += max * .01;
+  },
+});
+
+const generateYAxisStyle = (max) => ({
+  min: 0,
+  max,
+  ticks: {
+    beginAtZero: true,
+    font: {
+      family: STYLES.font,
+    },
+    color: 'black',
+    stepSize: 1,
+    autoSkip: false,
+  },
+  grid: {
+    color: 'black',
+    drawBorder: false,
+  },
+});
+
+const otherChartOptions = {
+  plugins: {
+    legend: false,
+  },
+  events: [],
+};
+
+const plugins = (linePositions, colorFn) => ([{
+  afterDatasetsDraw: function(chart) {
+    const ctx = chart.ctx;
+    const y_axis = chart.scales['y'];
+    const topY = y_axis.top;
+    const bottomY = y_axis.bottom;
+
+    ctx.lineWidth = 1;
+
+    linePositions.forEach((t, index) => {
+      const x = chart.scales.x.getPixelForValue(t)
+      ctx.save();
+      ctx.beginPath();
+      ctx.moveTo(x, topY);
+      ctx.lineTo(x, bottomY);
+      ctx.strokeStyle = colorFn(index);
+      ctx.stroke();
+      ctx.restore();
+    });
+  }
+}]);
+
+export default function ScoreTrend({
+  clueHistory,
+  guessEndTimes,
+  clueEndTimes,
+  annotationSuffix,
+  datasets,
+  indexToColor,
+  xMax,
+  yMax,
+}) {
+  return (
+    <Line
+      data={{
+        labels: generateLabels(guessEndTimes),
+        annotations: generateAnnotations(clueHistory, clueEndTimes, annotationSuffix),
+        datasets: datasets.map(d => generateDataset(d.name, d.color, d.trend)),
+      }}
+      plugins={plugins(guessEndTimes, indexToColor)}
+      options={{
+        ...otherChartOptions,
+        scales: {
+          x: generateXAxisStyle(
+            (context) => indexToColor(context.index),
+            xMax,
+          ),
+          y: generateYAxisStyle(yMax),
+        }
+      }}
+    />
+  );
+};
--- client/src/game_components/Stats.js
+++ client/src/game_components/Stats.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { LinearScale } from 'chart.js';
-import { Chart, Line } from 'react-chartjs-2';
+
+import ScoreTrend from '../components/ScoreTrend';
 
 import {
   isClassic,
@@ -8,174 +8,65 @@ import {
   STYLES,
 } from '../utils/const';
 
-class GameTimelineScale extends LinearScale {
-  buildTicks() {
-    return this.chart.data.annotations.map(v => ({
-      value: v.value,
-      label: [v.annotation],
-      major: false,
-    }));
-  }
-
-  generateTickLabels(ticks) {
-    // label already generated in buildTicks
-    // override for noop
-  }
-}
-
-GameTimelineScale.id = 'timelineScale';
-Chart.register(GameTimelineScale);
-
-const generateLabels = (guessEndTimes) => (
-  [0, ...guessEndTimes.map(t => Math.floor(t))]
-);
-
-const clueToAnnotation = (clue) => (`${clue.word} : ${clue.count}`);
-
-const generateAnnotations = (clueHistory, clueEndTimes, suffix) => {
-  const annotations = clueHistory.map(c => clueToAnnotation(c)).concat(suffix);
-  return clueEndTimes.map((t, i) => ({ value: t, annotation: annotations[i] }));
-};
-
-const generateDataset = (name, color, trend) => ({
-  label: name,
-  fill: false,
-  tension: 0.1,
-  backgroundColor: color,
-  borderColor: color,
-  borderWidth: 3,
-  data: trend,
-  pointRadius: 2,
-});
-
-const generateXAxisStyle = (color, max) => ({
-  min: 0,
-  max: max,
-  type: 'timelineScale',
-  ticks: {
-    padding: 0,
-    font: {
-      family: STYLES.font,
-      size: STYLES.fontSize,
-    },
-    color,
-    autoSkip: false,
-    maxRotation: 30,
-    minRotation: 30,
-    labelOffset: -STYLES.fontSize / 2,
-  },
-  grid: {
-    // display: false,
-    drawBorder: false,
-  },
-  afterDataLimits: function(axis) {
-    // add inner padding for points on the far right edge
-    const max = axis.max;
-    // console.log(axis.min, axis.max)
-    axis.min -= max * .01;
-    axis.max += max * .01;
-  },
-});
-
-const generateYAxisStyle = (max) => ({
-  min: 0,
-  max,
-  ticks: {
-    beginAtZero: true,
-    font: {
-      family: STYLES.font,
-    },
-    color: 'black',
-    stepSize: 1,
-    autoSkip: false,
-  },
-  grid: {
-    color: 'black',
-    drawBorder: false,
-  },
-  afterDataLimits: function(axis) {
-    // add innder padding for points on the top and bottom lines
-    axis.max += .5;
-    axis.min -= .5;
-  },
-});
-
-const otherChartOptions = {
-  plugins: {
-    legend: false,
-  },
-  events: [],
-};
-
 const secToTime = (s) => {
   if (s < 60 * 60) {
     return new Date(s * 1000).toISOString().substr(14, 5).toString();
   } else {
     return new Date(s * 1000).toISOString().substr(11, 8).toString();
   }
-}
+};
 
 export default function Stats({ mode, stats, clueHistory }) {
-  const secondTeam = stats.startTeam === 'blue' ? 'red' : 'blue';
-
   const renderClassicScoreTrend = (stats, clueHistory) => {
+    const secondTeam = stats.startTeam === 'blue' ? 'red' : 'blue';
+    const indexToColor = (index) => {
+      if (index % 2 === 0) {
+        return STYLES.colors[stats.startTeam];
+      } else {
+        return STYLES.colors[secondTeam];
+      }
+    };
+
     return (
-      <Line
-        data={{
-          labels: generateLabels(stats.guessEndTimes),
-          annotations: generateAnnotations(clueHistory, stats.clueEndTimes, []),
-          datasets: [
-            generateDataset(stats.startTeam, STYLES.colors[stats.startTeam], stats.firstTeamScoreTrend),
-            generateDataset(secondTeam, STYLES.colors[secondTeam], stats.secondTeamScoreTrend),
-          ],
-        }}
-        options={{
-          ...otherChartOptions,
-          scales: {
-            x: generateXAxisStyle(
-              (context) => {
-                if (context.index % 2 === 0) {
-                  return STYLES.colors[stats.startTeam];
-                } else {
-                  return STYLES.colors[secondTeam];
-                }
-              },
-              stats.timeInSec,
-            ),
-            y: generateYAxisStyle(9),
-          }
-        }}
+      <ScoreTrend
+        clueHistory={clueHistory}
+        guessEndTimes={stats.guessEndTimes}
+        clueEndTimes={stats.clueEndTimes}
+        annotationSuffix={[]}
+        datasets={[
+          { name: stats.startTeam, color: STYLES.colors[stats.startTeam], trend: stats.firstTeamScoreTrend },
+          { name: secondTeam, color: STYLES.colors[secondTeam], trend: stats.secondTeamScoreTrend },
+        ]}
+        indexToColor={indexToColor}
+        xMax={stats.timeInSec}
+        yMax={9}
       />
     );
   };
 
   const renderDuetScoreTrend = (stats, clueHistory) => {
+    const indexToColor = (index) => {
+      if (index < clueHistory.length) {
+        return STYLES.colors.green;
+      } else {
+        return 'black';
+      }
+    };
+
     const hasSuddenDeath = clueHistory.length === stats.scoreTrend.length - 2;
+
     return (
-      <Line
-        data={{
-          labels: generateLabels(stats.guessEndTimes),
-          annotations: generateAnnotations(clueHistory, stats.clueEndTimes, hasSuddenDeath ? ['sudden death'] : []),
-          datasets: [
-            generateDataset('green', STYLES.colors.green, stats.scoreTrend),
-          ],
-        }}
-        options={{
-          ...otherChartOptions,
-          scales: {
-            x: generateXAxisStyle(
-              (context) => {
-                if (context.index < clueHistory.length) {
-                  return STYLES.colors.green;
-                } else {
-                  return 'black';
-                }
-              },
-              stats.timeInSec,
-            ),
-            y: generateYAxisStyle(15),
-          }
-        }}
+      <ScoreTrend
+        clueHistory={clueHistory}
+        guessEndTimes={stats.guessEndTimes}
+        clueEndTimes={stats.clueEndTimes}
+        annotationSuffix={hasSuddenDeath ? ['sudden death'] : []}
+        datasets={[
+          { name: 'green', color: STYLES.colors.green, trend: stats.scoreTrend },
+        ]}
+        indexToColor={indexToColor}
+        xMax={stats.timeInSec}
+        yMax={15}
       />
     );
   };
