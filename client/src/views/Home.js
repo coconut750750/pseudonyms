6ba74077da63d6ecf6d47bd8263b40e9add24a66
Brandon Wang <sourbrandon@gmail.com>
2021-09-18 18:14:08 -0700

    update new features header

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -12,8 +12,8 @@ function Home(props) {
         <div className="col-md-6 text-center">
           <div className="alert alert-success updates" role="alert">
             <p>New features!</p>
-            <p>Clue history: Added a new component next to the board to track the clues that have been given. </p>
-            <p style={{color: 'black'}}>Dark Mode!!! Try the toggle in the upper right corner.</p>
+            <p>At the end of the game, we'll show you how the game played out. 
+            See what the best clues were, which turns took the longest, or when the losing team caught up! </p>
           </div>
         </div>
         <div className="col-md-6 text-center">
