046dd04714bccb2c8a15df23b6aa6f3bda6e8791
Brandon Wang <sourbrandon@gmail.com>
2024-12-09 23:04:16 -0800

    implement rejoin on client

---
+++ client/src/components/GameForm/Rejoin.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+
+import { checkName } from '../../api/register';
+import { CLASSIC, DUET } from '../..//utils/const';
+
+function Rejoin({
+  join,
+}) {
+  const [rejoinData, setRejoinData] = useState(undefined);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const { gameCode, name } = JSON.parse(localStorage.getItem("previous_game") || '{}');
+    if (gameCode === undefined || name === undefined) {
+      return;
+    }
+    checkName(name, gameCode).then(res => {
+      setRejoinData({ gameCode, name, mode: res.mode });
+    }).catch(res => {
+      localStorage.setItem('previous_game', JSON.stringify('{}'));
+    });
+
+  }, []);
+
+  const joinGame = async () => {
+    checkName(rejoinData.name, rejoinData.gameCode).then(res => {
+      join(rejoinData.gameCode, rejoinData.name, rejoinData.mode);
+    }).catch(res => {
+      setError(res.message);
+    });
+  };
+
+  if (rejoinData === undefined) {
+    return <></>;
+  }
+
+  return (
+    <div className="game-form elevated">
+      <div className="tabs row d-flex justify-content-center">
+        <div className="col tab selected">
+          <h6 className="tab-label">Rejoin</h6>
+        </div>
+      </div>
+      <div className="tab-content row d-flex justify-content-center">
+        <div className="form">
+            <h6 className="pt-2">{rejoinData.mode === CLASSIC ? "Classic" : "Duet"}</h6>
+            <h6 className="pt-2">Game Code: {rejoinData.gameCode}</h6>
+            <h6 className="pt-2">Name: {rejoinData.name}</h6>
+
+            <p className="form-error">{error}</p>
+            <button onClick={joinGame} className="btn mb-2">Rejoin</button>
+        </div>
+      </div>  
+    </div>
+  );
+}
+
+export default Rejoin;
--- client/src/components/GameForm/index.js
+++ client/src/components/GameForm/index.js
@@ -4,11 +4,19 @@ import './GameForm.css';
 
 import Create from './Create';
 import Join from './Join';
+import Rejoin from './Rejoin';
 
+export default function GameForm({
+  urlCode,
+  setGame,
+}) {
+  const setGameWrapper = (gameCode, name, mode) => {
+    localStorage.setItem('previous_game', JSON.stringify({ gameCode, name }));
+    setGame(gameCode, name, mode);
+  };
 
-export default function GameForm(props) {
-  const create = <Create setGame={props.setGame}/>;
-  const join = <Join urlGameCode={props.urlCode} join={props.setGame}/>;
+  const create = <Create setGame={setGameWrapper}/>;
+  const join = <Join urlGameCode={urlCode} join={setGameWrapper}/>;
 
   const tabLabels = ["Create", "Join"];
   const tabs = [create, join];
@@ -16,27 +24,32 @@ export default function GameForm(props) {
   const [tab, setTab] = useState(0);
 
   useEffect(() => {
-    if (props.urlCode !== undefined) {
+    if (urlCode !== undefined) {
       setTab(1);
     }
-  }, [props.urlCode]);
+  }, [urlCode]);
 
   return (
-    <div className="game-form elevated">
-      <div className="tabs row d-flex justify-content-center">
-        {tabLabels.map((t, i) => (
-          <div key={t} className={`col tab ${tab === i ? "selected" : "unselected"}`} onClick={ () => setTab(i) }>
-            <h6 className="tab-label">{t}</h6>
-          </div>
-        ))}
+    <>
+      <Rejoin join={setGameWrapper}/>
+      <br/>
+
+      <div className="game-form elevated">
+        <div className="tabs row d-flex justify-content-center">
+          {tabLabels.map((t, i) => (
+            <div key={t} className={`col tab ${tab === i ? "selected" : "unselected"}`} onClick={ () => setTab(i) }>
+              <h6 className="tab-label">{t}</h6>
+            </div>
+          ))}
+        </div>
+        <div className="tab-content row d-flex justify-content-center">
+          {tabs.map((t, i) => (
+            <div key={i} className={`form ${tab === i ? "show" : "hidden"}`}>
+              {tab === i && t}
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="tab-content row d-flex justify-content-center">
-        {tabs.map((t, i) => (
-          <div key={i} className={`form ${tab === i ? "show" : "hidden"}`}>
-            {tab === i && t}
-          </div>
-        ))}
-      </div>
-    </div>
+    </>
   );
 }
